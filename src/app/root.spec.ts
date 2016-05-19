eeeda68bbc88d16632ad282b2e70cc52b2601f38
Peter Sandor <peter@petersandor.name>
2016-05-19 23:08:55 +0200

    Removed obsolete test

--- src/app/root.spec.ts
+++ src/app/root.spec.ts
@@ -13,14 +13,12 @@ import {MockBackend} from '@angular/http/testing';
 import {provide} from "@angular/core";
 
 // Load the implementations that should be tested
-import {Api} from './services/api/index';
 import {App} from './index';
 
 describe('App', () => {
   // provide our implementations or mocks to the dependency injector
   beforeEachProviders(() => [
     App,
-    Api,
     BaseRequestOptions,
     MockBackend,
     // Provide a mocked (fake) backend for Http
@@ -32,11 +30,7 @@ describe('App', () => {
     })
   ]);
 
-  it('should have an url', inject([App], (app:App) => {
-    expect(app.url).toEqual('//github.com/cmelion/generator-ng2-webpack');
-  }));
-
-  describe('pipes', function() {});
+   describe('pipes', function() {});
 
   describe('services', function() {
     require('./services/api/spec');
