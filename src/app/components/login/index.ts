e9ebe41f1d1b532cc81837add66604c2479495e7
Peter Sandor <peter@petersandor.name>
2016-05-19 23:18:54 +0200

    Updated login page with ng2 material components

--- src/app/components/login/index.ts
+++ src/app/components/login/index.ts
@@ -1,5 +1,6 @@
 /* beautify ignore:start */
 import {Component} from '@angular/core';
+import {FORM_DIRECTIVES} from '@angular/common';
 import {Router, CanActivate} from '@angular/router-deprecated';
 /* beautify ignore:end */
 
@@ -9,13 +10,17 @@ import {tokenNotExpired} from 'angular2-jwt';
 import {UserService} from '../../services/user-service';
 
 // Sub components
-import {MdInput} from '@angular2-material/input';
-import {MdCard} from '@angular2-material/card';
+import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
+import {MdToolbar} from '@angular2-material/toolbar';
 import {MdButton} from '@angular2-material/button';
 
 @Component({
     selector: 'login',
-    directives: [MdInput, MdCard, MdButton],
+    host: {
+        '[class.row]': 'true'
+    },
+    directives: [FORM_DIRECTIVES, MD_INPUT_DIRECTIVES, MD_CARD_DIRECTIVES, MdToolbar, MdButton],
     styles: [require('./style.scss').toString()],
     template: require('./template.html')
 })
@@ -26,7 +31,9 @@ export class Login {
         private router: Router
     ) {}
 
-    login(email, password) {
+    login(event, email, password) {
+        event.preventDefault();
+
         this.userService.login(email, password).subscribe((result) => {
             if (result) {
                 this.router.navigate(['Home']);
