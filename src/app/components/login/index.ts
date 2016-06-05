7d1b07299132540b5b6d90b3f9c4b88af15a336e
Peter Sandor <peter@petersandor.name>
2016-06-05 20:12:23 +0200

    Added loading bar for login component
    Closes #3

--- src/app/components/login/index.ts
+++ src/app/components/login/index.ts
@@ -14,18 +14,30 @@ import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
 import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
 import {MdToolbar} from '@angular2-material/toolbar';
 import {MdButton} from '@angular2-material/button';
+import {MdProgressBar} from '@angular2-material/progress-bar';
+import {NgClass} from '@angular/common';
 
 @Component({
     selector: 'login',
     host: {
         '[class.row]': 'true'
     },
-    directives: [FORM_DIRECTIVES, MD_INPUT_DIRECTIVES, MD_CARD_DIRECTIVES, MdToolbar, MdButton],
+    directives: [
+        NgClass,
+        FORM_DIRECTIVES,
+        MD_INPUT_DIRECTIVES,
+        MD_CARD_DIRECTIVES,
+        MdToolbar,
+        MdButton,
+        MdProgressBar
+    ],
     styles: [require('./style.scss').toString()],
     template: require('./template.html')
 })
 @CanActivate(() => !tokenNotExpired())
 export class Login {
+    isLoading: Boolean = false;
+
     constructor(
         private userService: UserService,
         private router: Router
@@ -33,11 +45,14 @@ export class Login {
 
     login(event, email, password) {
         event.preventDefault();
+        this.isLoading = true;
 
         this.userService.login(email, password).subscribe((result) => {
             if (result) {
                 this.router.navigate(['Home']);
             }
+
+            this.isLoading = false;
         });
     }
 }
